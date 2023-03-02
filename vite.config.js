8cb0a23ab7a9d97a48f35a58d220285dcf6881c1
shiizam <selftaughtcoder21@gmail.com>
2023-03-02 14:12:26 -0500

    fix default import of svgLoader

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-    defaultImport: 'url',
+  plugins: [
+    react(), svgLoader({
+      defaultImport: 'url',
+    }),
+  ],
 })
